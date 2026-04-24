a3b2349bc714f35782943a99b3c77c961960dfa9
AltJawga <cda0044@auburn.edu>
2026-04-24 18:39:38 -0500

    Input formatting

--- run.sh
+++ run.sh
@@ -47,13 +47,16 @@ pip3 install -r ./requirements.txt
 # This is based off of the examples given in task-4, which state that
 # "the TA will provide nine inputs each of which includes two PDF files:"
 # with the example input of "cis-r1.pdf and cis-r2.pdf"
-echo "For each input combination, separate file paths with 'and'."
-echo "Ex: './cis-r1.pdf and ./cis-r2.pdf'"
+echo
+echo "Enter the input combinations to be sent to the python program"
+echo "   For each input combination, separate file paths with 'and'."
+echo "   Ex: './cis-r1.pdf and ./cis-r2.pdf'"
+echo
 
 # Loop through the program to run it 9 times, each with unique input
 for i in {1..9};
 do
-  read -a input_array -p "Enter input combination $i:   "
+  read -a input_array -p "   Enter input combination $i:   "
   echo ${input_array[0]}
   echo ${input_array[2]}
 done
